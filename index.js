f37d12d2f2e4857874cc79fdc8cf26f55a82f273
Alin <orianalin9@gmail.com>
2021-01-19 18:53:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const numarMare = (x) =>{
+    switch (x){
+        case 'Sergiu':
+            return 'It-ist'
+        case 'Alin':
+            return 'Mai mare ca sergiu'
+    }
+}
+console.log(numarMare('Alin'));
