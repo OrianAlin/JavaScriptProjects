a1d69944d6a9d659cbd1edcb320f175b7571d86e
Alin <orianalin9@gmail.com>
2021-02-01 03:22:42 +0200

    ssss

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const numarDouaCifre = (x) =>{
-    if (x>9 && x<100){
-        return "Numar de Doua Cifre"
-    } else {
-        return "Alt Numar";
-    }
-    }
-
+const numarDouaCifre = (x) => {
+  if (x > 9 && x < 100) {
+    return "Numar de Doua Cifre";
+  } else {
+    return "Alt Numar";
+  }
+};
 console.log(numarDouaCifre(34));
